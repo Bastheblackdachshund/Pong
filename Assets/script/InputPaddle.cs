1607ba6a4bace78abfc78574718beba96d28c3db
Bastheblackdachshund <144002097+Bastheblackdachshund@users.noreply.github.com>
2023-10-05 12:31:02 +0200

    Pong 5-10-23

--- Assets/script/InputPaddle.cs
+++ Assets/script/InputPaddle.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class InputPaddle : MonoBehaviour
 {
+    public float speed = 3f;
+    public string WestOrEast;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (WestOrEast == "West")
         {
-            Debug.Log("yes it is pressed");
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+                Debug.Log("Yes w is pressed");
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+                Debug.Log("Yes S is pressed");
+            }
+        }
+        else if (WestOrEast == "East")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+                Debug.Log("Yes up is pressed");
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+                Debug.Log("Yes down is pressed");
+            }
         }
     }
 }
+
